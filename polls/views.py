27e923f41b280bf6f0ea612bb1644f57995aa1e4
Erik Everton Rodrigues <erik.everton444@gmail.com>
2023-12-26 11:18:46 -0300

    Tests and static files are done

--- polls/views.py
+++ polls/views.py
@@ -7,6 +7,8 @@ from django.views import generic
 
 from .models import Choice, Question
 
+from django.utils import timezone
+
 # Create your views here.
 
 class IndexView(generic.ListView):
@@ -14,12 +16,15 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
